4cfdedc432708c5be8574c4911a87ab25fd91abf
Martin Go <martinkyle_go@yahoo.com>
2020-09-12 17:36:49 +0800

    completed read excel

--- hira kana training.py
+++ hira kana training.py
@@ -1,11 +1,19 @@
-import xlrd 
+import openpyxl
+from openpyxl import load_workbook
 
+#reading excel
 loc = ("drill list.xlsx") 
-wb = xlrd.open_workbook(loc) 
-sheet = wb.sheet_by_index(0) 
-  
-# For row 0 and column 0 
-print(sheet.cell_value(1, 1)) 
+wBook = load_workbook(loc) 
+wSheet=wBook['Sheet1']
+rows = wSheet.max_row
+columns = wSheet.max_column
 
-#print(u"あ")
-
+#read cell per cell
+ForShowing = input("column number to be shown")
+ForTesting = input("column number for testing")
+show = []
+testing = []
+for rownum in range(rows):	
+	show.append((wSheet.cell(row=rownum+1, column=int(ForShowing))).value)
+	testing.append((wSheet.cell(row=rownum+1, column=int(ForTesting))).value)
+	
