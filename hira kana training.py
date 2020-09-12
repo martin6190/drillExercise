3c0ffba0d202cc25f0485fae40b9723974d10b4d
Martin Go <martinkyle_go@yahoo.com>
2020-09-12 16:31:33 +0800

    added design doc

--- hira kana training.py
+++ hira kana training.py
@@ -1,2 +1,11 @@
-print　("あ")
+import xlrd 
+
+loc = ("drill list.xlsx") 
+wb = xlrd.open_workbook(loc) 
+sheet = wb.sheet_by_index(0) 
+  
+# For row 0 and column 0 
+print(sheet.cell_value(1, 1)) 
+
+#print(u"あ")
 
